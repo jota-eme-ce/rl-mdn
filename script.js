2b0683bbf41fc5f84a0f8251d93f7e35da22e335
Javier Martínez <jmartinez@adaptavist.com>
2025-05-03 18:41:12 +0200

    Red version

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const coin = document.getElementById('coin');
 const button = document.getElementById('flipBtn');
 
 const message = document.createElement('div');
-message.innerHTML = "<span class='typing-text'>¡El año que viene habrá un capillo verde más!</span>";
+message.innerHTML = "<span class='typing-text'>¡El año que viene habrá un capillo rojo más!</span>";
 message.classList.add('reveal-message');
 message.style.textAlign = "center";
 message.style.display = "none";
@@ -15,7 +15,7 @@ button.addEventListener('click', () => {
   void coin.offsetWidth;
 
   // Para terminar en la cruz: +180
-  currentRotation += 720 + 180;
+  currentRotation += 720;
 
   coin.style.transition = 'transform 7s ease-out';
   coin.style.transform = `rotateY(${currentRotation}deg)`;
