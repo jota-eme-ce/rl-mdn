24c266065f5ab0168c32a8f9f1d77e4c07ec2c14
Javier Martínez <jmartinez@adaptavist.com>
2025-05-03 17:57:48 +0200

    Responsive version

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const coin = document.getElementById('coin');
 const button = document.getElementById('flipBtn');
 
+const message = document.createElement('div');
+message.textContent = "¡!";
+message.classList.add('reveal-message');
+message.style.display = "none";
+document.querySelector('.container').appendChild(message);
+
 let currentRotation = 0;
 
 button.addEventListener('click', () => {
-  // Detener animación infinita
   coin.style.animation = 'none';
   void coin.offsetWidth;
 
-  // Siempre terminar en la cara (0°)
+  // Para terminar en la cruz: +180
   currentRotation += 720 + 180;
 
-  // Animar giro hasta la cara
   coin.style.transition = 'transform 5s ease-out';
   coin.style.transform = `rotateY(${currentRotation}deg)`;
+
+  setTimeout(() => {
+    message.style.display = "block";
+    message.classList.remove('reveal-message');
+    void message.offsetWidth;
+    message.classList.add('reveal-message');
+  }, 5000);
 });
